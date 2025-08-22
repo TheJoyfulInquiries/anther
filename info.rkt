01e33ec80d3c0adfa0f1368523457716099979f9
JoyInq <joy@joyinq.xyz>
2025-08-22 15:10:49 -0500

    working setup w/ --home-dir option

--- info.rkt
+++ info.rkt
@@ -1,9 +1,12 @@
 #lang info
 (define collection "anther")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "toml"))
 (define scribblings '(("scribblings/anther.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "(An)o(ther) Pollen Toolkit")
 (define version "0.0")
-(define pkg-authors '(asoteiros))
+(define pkg-authors '(TheJoyfulInquiries))
 (define license '(Apache-2.0 OR MIT))
+
+(define raco-bin-paths
+  '(("anther" "main.rkt")))
--- main.rkt
+++ main.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require rackunit racket/system racket/cmdline)
 
 ;; Notice
 ;; To install (from within the package directory):
@@ -24,27 +23,3 @@
 ;; http://docs.racket-lang.org/style/index.html
 
 ;; Code here
-
-
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
-
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
